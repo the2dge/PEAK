510813229367c19eadbef227f6fbb6264cff283b
the2dge <the2dge@users.noreply.github.com>
2023-04-30 16:26:54 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
 navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
 navbarMenu.addEventListener('click', toggleNavbarVisibility);
 
+document.addEventListener('DOMContentLoaded', function() {
+  const searchBox = document.getElementById('search-box');
+  searchBox.addEventListener('input', searchImages);
+});
+
+function searchImages() {
+  const searchTerm = document.getElementById("search-box").value.toLowerCase();
+  const images = document.querySelectorAll(".activity-child img");
+
+  for (let i = 0; i < images.length; i++) {
+    const img = images[i];
+    const altText = img.getAttribute("alt").toLowerCase();
+
+    if (altText.includes(searchTerm)) {
+      img.parentElement.style.display = "block";
+    } else {
+      img.parentElement.style.display = "none";
+    }
+  }
+}
+
+
